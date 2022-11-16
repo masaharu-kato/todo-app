517508253f53e8e701070e8a8c8a0d83c9b57251
Masaharu Kato <fudai.mk@gmail.com>
2022-11-16 17:07:28 +0900

    implement task listing and adding

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -5,11 +5,15 @@ import TaskAdd from '@/components/components/TaskAdd.vue'
 import TaskList from '@/components/components/TaskList.vue'
 import { Task } from '@/models/task';
 
-const addNewTask = (text: string) => {
+const addNewTask = async (text: string) => {
   if (!text) {
     window.alert('内容が入力されていません！')
     return
   }
+  await apiClient.post('/api/add-task', {
+    text: text
+  })
+  tasks.value = (await apiClient.get('/api/tasks')).data
   // tasksStore.addNewTask(text)
 }
 
@@ -22,9 +26,7 @@ const tasks = ref<Task[]>([])
 onMounted(async () => {
   console.log('onMounted')
   console.log('baseURL:', apiClient.defaults.baseURL)
-  const response = await apiClient.get('/api/tasks')
-  console.log('response:', response)
-  tasks.value = response.data
+  tasks.value = (await apiClient.get('/api/tasks')).data
 })
 
 </script>
